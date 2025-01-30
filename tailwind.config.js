5e0ebcf484fb80bd86e381eb9bf58901166bc4b6
mariodevat <mario.valdez@alltrade.mx>
2025-01-30 14:43:41 -0700

    Add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    fontFamily: {
-      'epilogue': ['Epilogue', 'system-ui']
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: "class",
+    theme: {
+        fontFamily: {
+            epilogue: ["Epilogue", "system-ui"],
+        },
+        screens: {
+            sm: "375px",
+            // => @media (min-width: 375px) { ... }
+            lg: "1440px",
+            // => @media (min-width: 1440px) { ... }
+        },
+        extend: {},
     },
-    screens: {
-      'sm': '375px',
-      // => @media (min-width: 375px) { ... }
-      'lg': '1440px',
-      // => @media (min-width: 1440px) { ... }
-    },
-    extend: {},
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
